b45737363bfa15d656d4958d3a6016ffe78d5b15
liubing <liubing01.shouqianba.com>
2024-09-19 02:02:30 +0800

    week 1 day 6 task 1

--- mini-lsm-starter/src/tests.rs
+++ mini-lsm-starter/src/tests.rs
@@ -1,6 +1,7 @@
 //! DO NOT MODIFY -- Mini-LSM tests modules
 //! This file will be automatically rewritten by the copy-test command.
 
+mod week1_day6;
 mod week1_day2;
 mod week1_day3;
 mod harness;
